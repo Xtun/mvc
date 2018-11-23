893bde4ef581eef0cfa3e5715fab2998359af03f
Xtun <inc.work@yandex.ru>
2018-11-23 15:50:42 +0300

    update mvc

---
+++ system/model.php
@@ -0,0 +1,26 @@
+<?
+namespace system;
+
+use \system\Database as DB;
+
+/*
+
+Use model 
+
+*/
+
+class Model
+{
+    protected $db;
+
+    function __construct(){
+        
+        $this->db=new DB();
+
+    }
+
+	public function get_data()
+	{
+		// todo
+	}
+}
---
+++ system/view.php
@@ -0,0 +1,25 @@
+<?php
+namespace system;
+
+class View
+{
+	/*
+     class for connection view failes
+    */
+	
+
+	public function generate($content_view, $data)
+	{
+		
+        
+		if(is_array($data)) {
+			
+			
+			extract($data);
+		}
+        
+		
+		include_once 'application/views/'.$content_view;
+        
+	}
+}
